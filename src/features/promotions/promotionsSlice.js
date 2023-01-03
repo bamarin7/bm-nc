f79099ba097df56314c02bd579833490355ef713
Brian Marin Silva <marinbrian.as@gmail.com>
2023-01-03 09:53:01 -0600

    Workshop 5 update

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -2,6 +2,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // import { PROMOTIONS } from '../../app/shared/PROMOTIONS';
 import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from "../../utils/mapImageURL";
 
 export const fetchPromotions = createAsyncThunk(
     'promotions/fetchPromotions',
